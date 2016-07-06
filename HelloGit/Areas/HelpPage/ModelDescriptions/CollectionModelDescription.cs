a5e72c826c658808d77e2a6dcf44061a7e81bd84
Vincent Yang <vincent.yang@vedaleon.com>
2016-07-06 10:26:44 +1000

    Init version of HelloGit project for .NET VS 2013

---
+++ HelloGit/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace HelloGit.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ HelloGit/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HelloGit.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
